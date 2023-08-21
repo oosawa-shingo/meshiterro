e669e3cf833591848c3289de6a04243dd82d8ffa
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2023-08-21 14:58:07 +0000

    [Finish] 4-13

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  devise_for :users
+  root to: "homes#top"
+  get '/homes/about' => "homes#about", as:"about"
+end
